9e6c896d5072bc137b5ca699536d0055fd5dccd0
Henry Alpert <31975130+halpert3@users.noreply.github.com>
2022-07-22 13:46:04 -0500

    Update streamlit_app.py
    Added smoothie item

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,5 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Eggs')
 streamlit.text('Pancakes')
 streamlit.text('Hash browns')
+
+streamlit.header('🍌Banana Smoothies Available + Your Choice of Berry 🫐🍓')
