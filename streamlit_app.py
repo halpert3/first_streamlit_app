aab6851a7566d7fd6ee324ef9a890c0872df37c9
Henry Alpert <31975130+halpert3@users.noreply.github.com>
2022-07-22 14:04:15 -0500

    Update streamlit_app.py
    Fixed index

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 import pandas as pd 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 streamlit.title('Healthy Diner')
 
@@ -10,7 +11,7 @@ streamlit.text('Pancakes')
 streamlit.text('Hash browns')
 
 streamlit.header('🍌Banana Smoothies Available + Your Choice of Additional Fruit 🫐🍓')
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.fruit))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 
 streamlit.dataframe(my_fruit_list)
